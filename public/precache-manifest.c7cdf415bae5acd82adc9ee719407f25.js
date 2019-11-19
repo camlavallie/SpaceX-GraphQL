0f1475b66c6d6cc59fdf5c026e60d30be8d68502
Cameron LaVallie <camlavallie@gmail.com>
2019-11-19 15:13:00 -0600

    Finished GraphQL/React Project.

---
+++ public/precache-manifest.c7cdf415bae5acd82adc9ee719407f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a670e6bdf6519b747276244db60f352a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75abbf892ab645c06c69",
+    "url": "/static/css/2.a6756767.chunk.css"
+  },
+  {
+    "revision": "852b02a08b15383f775b",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "75abbf892ab645c06c69",
+    "url": "/static/js/2.2e9d101b.chunk.js"
+  },
+  {
+    "revision": "852b02a08b15383f775b",
+    "url": "/static/js/main.ed8c184b.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "a88f20cb395b408e49dc6153ff5f090b",
+    "url": "/static/media/logo.a88f20cb.png"
+  }
+]);
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
-const app = express();
+const cors = require('cors');
 const schema = require('./schema');
+const path = require('path');
+const app = express();
+// Allow cors-origin
+app.use(cors());
+
 app.use(
   '/graphql',
   graphqlHTTP({
@@ -9,6 +14,12 @@ app.use(
     graphiql: true,
   }),
 );
+
+app.use(express.static('public'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+})
 
 const PORT = process.env.PORT || 5000;
 
